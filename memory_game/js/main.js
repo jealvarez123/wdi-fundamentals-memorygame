ec2d53183216a914fdfdec529a652435a8c2f9ac
jealvarez123 <jealvarez123@gmail.com>
2018-03-10 21:09:57 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,4 @@
 
-/*
- var cards = ["queen", "queen", "king", "king"];
-
- var cardsInPlay = [];
-
- var cardOne = cards[0];
-
-console.log("User flipped" + " " + cardOne);
-
-var cardTwo = cards[2];
-
-console.log("User flipped" + " " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-
-} else if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else { 
-	alert("Sorry, try again!");
-}
-*/
 
 var cards = ["queen", "queen", "king", "king"];
 
@@ -35,14 +14,17 @@ var checkForMatch = function() {
 };
 
 if (cardsInPlay.length === 2) {
-	checkForMatch();
-}
+	
+};
 
 var flipCard = function(cardId) {
+
 
 	console.log("User flipped " + cards[cardId]);
 
 	cardsInPlay.push(cards[cardId]);
+checkForMatch();
+	
 };
 
 flipCard(0);
